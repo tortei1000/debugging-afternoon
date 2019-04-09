c907b8c2e4acbefeee247ccdcdea1fc9f86c6f7e
tiago maffei <tiagomaffei@gmail.com>
2019-04-09 13:14:08 -0600

    fixed multiple bugs

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
+import axios from "axios"
 
 class App extends Component {
   constructor(props) {
@@ -54,9 +55,9 @@ class App extends Component {
         <NavBar navigate={this.navigate} />
         <div className="main-container">
           {showCart ? (
-            <ShoppingCart cart={cart} />
+            <ShoppingCart cart={this.state.cart} removeFromCart={this.removeFromCart}/>
           ) : (
-            <StoreFront products={products} />
+            <StoreFront products={products} addToCart={this.addToCart}/>
           )}
         </div>
       </div>
